012c249277ac13c14e52f04b516683cca6550bd5
Remi <rkuba@mit.edu>
2024-11-25 16:08:45 -0500

    sad basecode and test

--- controller_fpga/hdl/sad.sv
+++ controller_fpga/hdl/sad.sv
@@ -5,24 +5,143 @@
 module sad (
     input wire clk_in,
     input wire rst_in,
-    input wire [KERNEL_SIZE-1:0][7:0] data_cam1,
-    input wire [KERNEL_SIZE-1:0][7:0] data_cam2,
+    input wire [KERNEL_SIZE-1:0][7:0] left_data_in,
+    input wire [KERNEL_SIZE-1:0][7:0] right_data_in,
     input wire [10:0] hcount_in,
     input wire [9:0] vcount_in,
     input wire data_valid_in,
     output logic data_valid_out,
-    output logic [10:0] hcount_out,
-    output logic [9:0] vcount_out,
+    output logic busy_out, // whether it is still calculating a module
+    output logic [9:0] hcount_out,
+    output logic [8:0] vcount_out,
     output logic [7:0] line_out
     );
 
     localparam KERNEL_SIZE = 3;
+    localparam OFFSET_BLOCK_SIZE = OFFSET * KERNEL_SIZE;
+    localparam BLOCK_SIZE = OFFSET * OFFSET;
+    localparam LEFT_CACHE_SIZE = KERNEL_SIZE * KERNEL_SIZE;
+    localparam RIGHT_CACHE_SIZE = (KERNEL_SIZE+OFFSET)*KERNEL_SIZE;
     localparam OFFSET = 10;
 
-    logic signed [KERNEL_SIZE-1:0][KERNEL_SIZE-1:0][7:0] coeffs;
+    // make cache elements 9 bits to hold a sign bit at top
+    logic [LEFT_CACHE_SIZE-1:0][8:0] left_cache;
+    logic [RIGHT_CACHE_SIZE-1:0][8:0] right_cache;
+    logic [$clog2(OFFSET)-1:0] curr_offset;
+    logic [$clog2(OFFSET)-1:0] best_offset;
+    logic [$clog2(OFFSET*KERNEL_SIZE)-1:0] block_offset;
+    logic [11:0] smallest_diff; // This should be unsigned
+    logic [OFFSET-1:0][10:0] hcount_pl;
+    logic [OFFSET-1:0][9:0] vcount_pl;
+
+    // FOR TESTING PUPROSES
+    logic [$clog2(BLOCK_SIZE)+7:0] recent_diff; // 7 for 255 color
+
+
 
     always_ff @(posedge clk_in) begin
-        if (data_valid_in) begin
+        if (rst_in) begin
+            left_cache <= 0;
+            right_cache <= 0;
+            curr_offset <= OFFSET;
+            best_offset <= OFFSET;
+            smallest_diff <= ~0; 
+            hcount_pl <= 0;
+            vcount_pl <= 0;
+            block_offset <= 0;
+            recent_diff <= 0;
+            
+            data_valid_out <= 1'b0;
+            busy_out <= 1'b0;
+            hcount_out <= 10'b0;
+            vcount_out <= 9'b0;
+            line_out <= 8'b0;
+        end else if (busy_out) begin
+            busy_out <= (curr_offset != 0);
+            curr_offset <= (curr_offset - 1 >= 0)? curr_offset - 1 : OFFSET;
+            data_valid_out <= (curr_offset == 0);
+            block_offset <= block_offset - KERNEL_SIZE;
+
+            for (int i = 1; i < OFFSET; i = i + 1) begin
+                hcount_pl[i] <= hcount_pl[i - 1];
+                vcount_pl[i] <= vcount_pl[i - 1];
+            end
+
+    
+            if ((
+                (left_cache[0] - right_cache[block_offset])[7:0] + 
+                (left_cache[1] - right_cache[block_offset+1])[7:0] + 
+                (left_cache[2] - right_cache[block_offset+2])[7:0] + 
+                (left_cache[3] - right_cache[block_offset+3])[7:0] + 
+                (left_cache[4] - right_cache[block_offset+4])[7:0] + 
+                (left_cache[5] - right_cache[block_offset+5])[7:0] + 
+                (left_cache[6] - right_cache[block_offset+6])[7:0] + 
+                (left_cache[7] - right_cache[block_offset+7])[7:0] + 
+                (left_cache[8] - right_cache[block_offset+8])[7:0] 
+                ) < smallest_diff) begin
+                smallest_diff <= (
+                    (left_cache[0] - right_cache[block_offset])[7:0] + 
+                    (left_cache[1] - right_cache[block_offset+1])[7:0] + 
+                    (left_cache[2] - right_cache[block_offset+2])[7:0] + 
+                    (left_cache[3] - right_cache[block_offset+3])[7:0] + 
+                    (left_cache[4] - right_cache[block_offset+4])[7:0] + 
+                    (left_cache[5] - right_cache[block_offset+5])[7:0] + 
+                    (left_cache[6] - right_cache[block_offset+6])[7:0] + 
+                    (left_cache[7] - right_cache[block_offset+7])[7:0] + 
+                    (left_cache[8] - right_cache[block_offset+8])[7:0] );
+                best_offset <= curr_offset;
+            end
+            recent_diff <= (
+                (left_cache[0] - right_cache[block_offset])[7:0] + 
+                (left_cache[1] - right_cache[block_offset+1])[7:0] + 
+                (left_cache[2] - right_cache[block_offset+2])[7:0] + 
+                (left_cache[3] - right_cache[block_offset+3])[7:0] + 
+                (left_cache[4] - right_cache[block_offset+4])[7:0] + 
+                (left_cache[5] - right_cache[block_offset+5])[7:0] + 
+                (left_cache[6] - right_cache[block_offset+6])[7:0] + 
+                (left_cache[7] - right_cache[block_offset+7])[7:0] + 
+                (left_cache[8] - right_cache[block_offset+8])[7:0] );
+
+        end else if (data_valid_in) begin
+            hcount_pl[0] <= hcount_in;
+            vcount_pl[0] <= vcount_in;
+            busy_out <= 1'b1;
+
+            for (int i = 0; i < LEFT_CACHE_SIZE-3; i++) begin
+                left_cache[i+3] <= left_cache[i];
+            end
+            for (int i = 0; i < RIGHT_CACHE_SIZE-3; i++) begin
+                right_cache[i+3] <= right_cache[i];
+            end
+            left_cache[2] <= {1'b0, left_data_in[0]};
+            left_cache[1] <= {1'b0, left_data_in[1]};
+            left_cache[0] <= {1'b0, left_data_in[2]};
+            right_cache[2] <= {1'b0, right_data_in[0]};
+            right_cache[1] <= {1'b0, right_data_in[1]};
+            right_cache[0] <= {1'b0, right_data_in[2]};
+
+            smallest_diff <= (  (left_cache[5] - right_cache[38])[7:0] +
+                                (left_cache[4] - right_cache[37])[7:0] + 
+                                (left_cache[3] - right_cache[36])[7:0] +
+                                (left_cache[2] - right_cache[35])[7:0] +
+                                (left_cache[1] - right_cache[34])[7:0] + 
+                                (left_cache[0] - right_cache[33])[7:0] +
+                                ({1'b0, left_data_in[0]} - right_cache[32])[7:0] +
+                                ({1'b0, left_data_in[1]} - right_cache[31])[7:0] +
+                                ({1'b0, left_data_in[2]} - right_cache[30])[7:0]);
+            recent_diff <= (    (left_cache[5] - right_cache[38])[7:0] +
+                                (left_cache[4] - right_cache[37])[7:0] + 
+                                (left_cache[3] - right_cache[36])[7:0] +
+                                (left_cache[2] - right_cache[35])[7:0] +
+                                (left_cache[1] - right_cache[34])[7:0] + 
+                                (left_cache[0] - right_cache[33])[7:0] +
+                                ({1'b0, left_data_in[0]} - right_cache[32])[7:0] +
+                                ({1'b0, left_data_in[1]} - right_cache[31])[7:0] +
+                                ({1'b0, left_data_in[2]} - right_cache[30])[7:0]);
+            
+            best_offset <= OFFSET;
+            curr_offset <= curr_offset - 1;
+            block_offset <= OFFSET_BLOCK_SIZE;
         end
     end
 
---
+++ controller_fpga/sim/test_sad.py
@@ -0,0 +1,120 @@
+import cocotb
+import os
+import random
+import sys
+from math import log
+import logging
+from pathlib import Path
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, ClockCycles, RisingEdge, FallingEdge, ReadOnly,with_timeout
+from cocotb.utils import get_sim_time as gst
+from cocotb.runner import get_runner
+from PIL import Image
+import numpy as np
+
+def stereo_match(left_file, right_file, kernel, max_offset):
+    left_img = Image.open(left_file).convert('L')
+    left = np.asarray(left_img)
+    right_img = Image.open(right_file).convert('L')
+    right = np.asarray(right_img) 
+    w, h = left_img.size
+    depth = np.zeros((w, h), np.uint8)
+    depth.shape = h, w
+
+    kernel_half = int(kernel / 2)
+    offset_adjust = 255 / max_offset  # this is used to map depth map output to 0-255 range
+      
+    for y in range(kernel_half, h - kernel_half):              
+        for x in range(kernel_half, w - kernel_half):
+            best_offset = 0
+            prev_sad = 65534
+            
+            for offset in range(max_offset):               
+                sad = 0
+                sad_temp = 0
+                for v in range(-kernel_half, kernel_half):
+                    for u in range(-kernel_half, kernel_half):
+                        sad_temp = abs(int(left[y+v, x+u]) - int(right[y+v, (x+u) - offset]) )
+                        sad += sad_temp
+                if sad < prev_sad:
+                    prev_sad = sad
+                    best_offset = offset
+            depth[y, x] = best_offset * offset_adjust
+    return w, h, left_img, right_img, depth
+
+
+def line_out(img, x, y):
+    return img.getpixel((x,y+1)) << 16 | img.getpixel((x,y)) << 8 | img.getpixel((x,y-1))
+
+
+@cocotb.test()
+async def test_sad(dut):
+    """cocotb test for sad calculation"""
+    dut._log.info("Starting...")
+    cocotb.start_soon(Clock(dut.clk_in, 10, units="ns").start())
+    dut._log.info("Holding reset...")
+    dut.rst_in.value = 1
+    await ClockCycles(dut.clk_in, 3, rising = False)
+
+    w, h, left_img, right_img, depth = stereo_match("../../data/Tsukuba_L.png", "../../data/Tsukuba_R.png", 3, 10)
+
+    dut.rst_in.value = 0
+    await ClockCycles(dut.clk_in, 3, rising = False)
+    # for y in range(1, h-1):
+    #     for x in range(w): 
+    for y in range(1, 3):
+        for x in range(w):
+            left_line = line_out(left_img, x, y)
+            right_line = line_out(right_img, x, y)
+            dut.hcount_in.value = x
+            dut.vcount_in.value = y
+            dut.left_data_in.value = left_line
+            dut.right_data_in.value = right_line
+            dut.data_valid_in.value = 1
+
+            if dut.data_valid_out.value == 1:
+                hc_out = dut.hcount_out.value
+                vc_out = dut.vcount_out.value
+                pixel = dut.line_out.value
+                dut._log.info(f"Pixel: {(hex(vc_out), hex(hc_out))} with grey: {hex(pixel)}")
+            
+            await ClockCycles(dut.clk_in, 1, rising=False)
+            dut.data_valid_in.value = 0
+            await ClockCycles(dut.clk_in, 12, rising=False)
+        
+
+
+
+
+
+
+def sad_runner():
+    """Simulate the counter using the Python runner."""
+    hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
+    sim = os.getenv("SIM", "icarus")
+    proj_path = Path(__file__).resolve().parent.parent
+    sys.path.append(str(proj_path / "sim" / "model"))
+    sources = [proj_path / "hdl" / "sad.sv"]
+    build_test_args = ["-Wall"]
+    parameters = {} 
+    sys.path.append(str(proj_path / "sim"))
+    runner = get_runner(sim)
+    runner.build(
+        sources=sources,
+        hdl_toplevel="sad",
+        always=True,
+        build_args=build_test_args,
+        parameters=parameters,
+        timescale = ('1ns','1ps'),
+        waves=True
+    )
+    run_test_args = []
+    runner.test(
+        hdl_toplevel="sad",
+        test_module="test_sad",
+        test_args=run_test_args,
+        waves=True
+    )
+
+if __name__ == "__main__":
+    sad_runner()
