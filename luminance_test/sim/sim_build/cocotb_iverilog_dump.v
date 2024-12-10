9ad159defb8fe6e5f7df933bcce1a84277625e34
Remi <rkuba@mit.edu>
2024-12-09 22:06:06 -0500

    Remove old directories

--- luminance_test/sim/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("/Users/remi/Desktop/lab05_partb/sim/sim_build/pixel_reconstruct.fst");
-    $dumpvars(0, pixel_reconstruct);
-end
-endmodule
--- sad_testing/sim/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("/Users/remi/Desktop/fpga-depth-mapping/sad_testing/sim/sim_build/sad.fst");
-    $dumpvars(0, sad);
-end
-endmodule
